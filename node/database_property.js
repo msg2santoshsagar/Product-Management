5e5ad03df9a17b55581ce61e28b837d95b68d585
santosh sagar <msg2santoshsagar@gmail.com>
2017-09-09 10:45:04 +0530

    Console added for printing db info

--- node/database_property.js
+++ node/database_property.js
@@ -5,6 +5,12 @@ const DB_USER 	= process.env.DBUSER 	|| 'root';
 const DB_PWD  	= process.env.DBPWD  	|| 'root';
 const DB_SCHEMA = process.env.DBSCHEMA  || 'product_management_local';
 
+console.log ("DATA BASE PROPERTY");
+console.info("DB_HOST 	-> ",DB_HOST);
+console.info("DB_USER 	-> ",DB_USER);
+console.info("DB_PWD  	-> ",DB_PWD);
+console.info("DB_SCHEMA ->  ",DB_SCHEMA);
+
 module.exports = {
 		DB_HOST 	: DB_HOST,
 		DB_USER 	: DB_USER,
