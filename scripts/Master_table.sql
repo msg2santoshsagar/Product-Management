d3649b385bda262d1b04f02e8db1ae4b540aeec8
santosh sagar <msg2santoshsagar@gmail.com>
2017-09-16 23:02:06 +0530

    Master table script updated
    TO support UTF-8
    Date to datetime

--- scripts/Master_table.sql
+++ scripts/Master_table.sql
@@ -4,49 +4,52 @@ CREATE SCHEMA `product_management_local` ;
 #REM creating user table 
 
 CREATE TABLE `user` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  `role` ENUM('ADMIN', 'SALESMAN') NULL,
-  `userid` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(100) NOT NULL,
-  `active` ENUM('YES','NO') NOT NULL,  
-  `createdBy` VARCHAR(45) NOT NULL,
-  `createdDate` DATE NOT NULL,
-  `updatedBy` VARCHAR(45) NOT NULL,
-  `updatedDate` DATE NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) CHARACTER SET latin1 NOT NULL,
+  `role` enum('ADMIN','SALESMAN') CHARACTER SET latin1 DEFAULT NULL,
+  `userid` varchar(45) CHARACTER SET latin1 NOT NULL,
+  `password` varchar(100) CHARACTER SET latin1 NOT NULL,
+  `active` enum('YES','NO') CHARACTER SET latin1 NOT NULL,
+  `createdBy` varchar(45) CHARACTER SET latin1 NOT NULL,
+  `createdDate` datetime NOT NULL,
+  `updatedBy` varchar(45) CHARACTER SET latin1 NOT NULL,
+  `updatedDate` datetime NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `userid_UNIQUE` (`userid` ASC));
+  UNIQUE KEY `userid_UNIQUE` (`userid`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
 
 #REM creating product table
   
  CREATE TABLE `product` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
+  `name` varchar(100) CHARACTER SET latin1 NOT NULL,
   `price` float NOT NULL DEFAULT '0',
   `current_stock` int(10) unsigned NOT NULL DEFAULT '0',
   `threshold_stock` int(10) unsigned NOT NULL DEFAULT '10',
-  `createdBy` varchar(45) NOT NULL,
-  `createdDate` date NOT NULL,
-  `updatedBy` varchar(45) NOT NULL,
-  `updatedDate` date NOT NULL,
+  `createdBy` varchar(45) CHARACTER SET latin1 NOT NULL,
+  `createdDate` datetime NOT NULL,
+  `updatedBy` varchar(45) CHARACTER SET latin1 NOT NULL,
+  `updatedDate` datetime NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `product_name_UNIQUE` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
 
 
 
 #REM creating product_order_history table
 
-  CREATE TABLE `product_order_history` (
+CREATE TABLE `product_order_history` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `product_id` int(11) NOT NULL,
   `quantity` int(10) unsigned NOT NULL DEFAULT '0',
   `price` float NOT NULL DEFAULT '0',
-  `createdBy` varchar(45) NOT NULL,
-  `createdDate` date NOT NULL,
-  `updatedBy` varchar(45) NOT NULL,
-  `updatedDate` date NOT NULL,
+  `createdBy` varchar(45) CHARACTER SET latin1 NOT NULL,
+  `createdDate` datetime NOT NULL,
+  `updatedBy` varchar(45) CHARACTER SET latin1 NOT NULL,
+  `updatedDate` datetime NOT NULL,
   PRIMARY KEY (`id`),
   KEY `product_id_fk_idx` (`product_id`),
   CONSTRAINT `product_order_history_product_id_fk` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+
