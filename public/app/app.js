c29fe72bf3ac4c47842348e11482332cb596020a
santosh sagar <msg2santoshsagar@gmail.com>
2017-10-11 14:30:12 +0530

    Notification module added

--- public/app/app.js
+++ public/app/app.js
@@ -11,17 +11,35 @@
 		'ui.grid.resizeColumns',
 		'ui.grid.edit',
 		'ui.grid.selection',
-		'ui.select'
+		'ui.select',
+		'ngDesktopNotification'
 		]);
 
 	app.config(config);
 	app.run(run);
 
-	run.$inject = ['stateHandler','AuthService'];
-	function run(stateHandler,AuthService) {
+	run.$inject = ['stateHandler','AuthService','desktopNotification'];
+	function run(stateHandler,AuthService,desktopNotification) {
 		stateHandler.initialize();
 		AuthService.getCurrentUser();
-		
+
+		if( desktopNotification.isSupported() ){
+			console.log("Current Permission : ",desktopNotification.currentPermission());
+
+			if( desktopNotification.currentPermission() ===  desktopNotification.permissions.granted){
+				console.info("Notification permission already available");
+			}else{
+				desktopNotification.requestPermission().then(function successCallback(){
+					console.info("User approved notification permisson");
+				}, function errorCallback(){
+					console.warn("User rejected notification permisson");
+				});
+			}
+
+		}else{
+			console.warn("This browser doesn't support Notification API");
+		}
+
 	}
 
 	config.$inject = ['$locationProvider'];
